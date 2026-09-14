1df3ec7ab7525a3d46361b1af979f926b38020b8
Thushar shetty <shettythushar31@gmail.com>
2026-09-14 16:46:04 +0530

    objects in ts

--- src/index.ts
+++ src/index.ts
@@ -57,3 +57,120 @@ let bookString='{"title":"The Great Gatsby"}';
 let book=JSON.parse(bookString) as Book;
 
 console.log(book.title);
+
+let value:any;
+
+value="thushar"
+value=[1,2,3];
+value=2.5;
+value.toUpperCase();
+
+let newValue:unknown;
+
+newValue="thushar"
+newValue=[1,2,3];
+newValue=2.5;
+if(typeof newValue === 'string'){
+newValue.toUpperCase();
+}
+
+const data:unknown="thejijDhwsdclknjs";
+const strData:string=data as string;
+
+interface Cupsize{
+    size:"small" |"large"
+}
+
+class chai implements Cupsize{
+    size: "small" | "large"="small"
+}
+
+type person1={height:number}
+type person2={weight:number}
+
+type person=person1 & person2;
+
+const thushar:person={
+    height:5.8,
+    weight:70
+}
+
+type user={
+    username:string,
+    bio?:string
+}
+const u1:user={
+    username:"thushar"
+}
+const u2:user={username:"thushar",bio:"I am a software engineer"}
+
+
+type config={
+    readonly appName:string,
+    version:number
+}
+const config1:config={
+    appName:"MyApp",
+    version:1.0
+}
+//config1.appName="NewApp"; // Error: Cannot assign to 'appName' because it is a read-only property.
+config1.version=2.0; // Allowed: 'version' is not read-only
+
+
+let tea:{
+    name:string,
+    price:number,
+    isHot:boolean
+    ingredients:string[]
+}={
+    name:"Green Tea",
+    price:2.5,
+    isHot:true,
+    ingredients:["Green tea leaves","Water"]//only string array is allowed
+}
+
+type  Personn={name:string};
+
+const p1={name:"thushar",age:25};
+const ps:Personn=p1; // allowed because p1 has at least the properties of Personn 
+
+
+type chai1={
+    name:string,
+    price:number,
+    isHot:boolean
+}
+const updateChai=(updates:Partial<chai1>)=>{
+    //updates can have any subset of properties of chai1    
+    console.log(updates);
+    }
+
+updateChai({name:"Black Tea"});
+
+
+type chaiOrder={
+    name?:string,
+    price?:number,
+}
+const placeOrder=(order:Required<chaiOrder>)=>{
+    //order must have all properties of chaiOrder
+    console.log(order);
+}
+
+placeOrder({name:"Black Tea",price:3.0});
+
+//pick
+
+type chaiDetails={
+    name:string,
+    price:number,
+    isHot:boolean,
+    ingredients:string[]
+}
+//pick allows you to create a new type by selecting specific properties from an existing type 
+type chaiSummary=Pick<chaiDetails,"name"|"price">;
+
+const summary:chaiSummary={
+    name:"Green Tea",
+    price:2.5
+}
